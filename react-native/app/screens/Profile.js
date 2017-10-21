4b7d32e826bc8c0352e598efd569f9097863f598
Frederico Teixeira <mrfrederico89@gmail.com>
2017-10-21 10:47:40 +0100

    User Accounts > Signing In - completed

--- react-native/app/screens/Profile.js
+++ react-native/app/screens/Profile.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
+import Meteor from 'react-native-meteor'
+import { NavigationActions } from 'react-navigation'
+
 import Container from '../components/Container'
+import { PrimaryButton } from '../components/Form'
 import { Header } from '../components/Text'
 
 class Profile extends Component {
+  signOut = () => {
+    Meteor.logout(() => {
+      const resetAction = NavigationActions.reset({
+        index: 0,
+        actions: [ NavigationActions.navigate({ routeName: 'SignUp' }) ],
+      })
+      this.props.navigation.dispatch(resetAction)
+    })
+  }
+
   render() {
     return (
       <Container>
         <Header>Profile</Header>
+        <PrimaryButton title='Sign Out' onPress={this.signOut} />
       </Container>
     )
   }
--- react-native/app/screens/SignIn.js
+++ react-native/app/screens/SignIn.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react'
-
+import Meteor from 'react-native-meteor'
 import { Card } from 'react-native-elements'
-import { Input, PrimaryButton } from '../components/Form'
+import { NavigationActions } from 'react-navigation'
 
+import { connectAlert } from '../components/Alert'
 import Container from '../components/Container'
+import { Input, PrimaryButton } from '../components/Form'
 
 class SignUp extends Component {
   state = {
     emailOrUsername: '',
     password: '',
+    loading: false,
+  }
+
+  signIn = () => {
+    const { emailOrUsername, password } = this.state
+
+    if (emailOrUsername.length === 0) {
+      return this.props.alertWithType(
+        'error',
+        'Error',
+        'Email or username is required.',
+      )
+    } else if (password.length === 0) {
+      return this.props.alertWithType('error', 'Error', 'Password is required.')
+    }
+
+    this.setState({ loading: true })
+    return Meteor.loginWithPassword(emailOrUsername, password, err => {
+      this.setState({ loading: false })
+      if (!err) {
+        const resetAction = NavigationActions.reset({
+          index: 0,
+          actions: [ NavigationActions.navigate({ routeName: 'Profile' }) ],
+        })
+        this.props.navigation.dispatch(resetAction)
+      } else {
+        this.props.alertWithType('error', 'Error', err.reason)
+      }
+    })
   }
 
   render() {
@@ -29,11 +60,15 @@ class SignUp extends Component {
             onChangeText={password => this.setState({ password })}
             value={this.state.password}
           />
-          <PrimaryButton title='Sign In' />
+          <PrimaryButton
+            title='Sign In'
+            onPress={this.signIn}
+            loading={this.state.loading}
+          />
         </Card>
       </Container>
     )
   }
 }
 
-export default SignUp
+export default connectAlert(SignUp)
